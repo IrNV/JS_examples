be13059cfcf7f085e04bfcd58ac019a8bac65f08
Valera <NsT.valerka@ukr.net>
2018-07-24 18:49:25 +0300

    add if-else example

---
+++ simple_examples/if_else.js
@@ -0,0 +1,23 @@
+﻿var userName = prompt("Кто пришел", "");
+
+if (userName == "Админ") {
+
+	var password = prompt("Пароль?", "");
+
+	if (password == "Черный Властелин") {
+		alert("Приветствую Вас!");
+	} else if (password == null) {
+		alert("Вход отменен");
+	} else {
+		alert("Пароль не верен");
+	}
+
+} else if (userName == null) {
+
+	alert("Вход отменен");
+
+} else {
+
+	alert("Я вас не знаю");
+
+}
