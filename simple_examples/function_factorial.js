f726eaaf7ac7857ac14f38abdeda0e27c52e9c6f
Valera <NsT.valerka@ukr.net>
2018-07-25 22:45:08 +0300

    add factorial function

---
+++ simple_examples/function_factorial.js
@@ -0,0 +1,13 @@
+/*
+	Функция вычисления факториала числа с помощью рекурсии.
+*/
+function factorial(n) {
+	
+	if (n) return n * factorial(n-1);
+
+	return 1;
+}
+
+n = +prompt("Введите число");
+
+alert(factorial(n));
