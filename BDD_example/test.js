e677be6c0ba1e02fe78c3376900bb9e81222b055
Valera <NsT.valerka@ukr.net>
2018-07-27 03:50:36 +0300

    Created function pow and tests

--- BDD_example/test.js
+++ BDD_example/test.js
@@ -1,14 +1,34 @@
 describe("pow", function() {
 
-  function makeTest(x) {
-    var expected = x * x * x;
-    it("при возведении " + x + " в степень 3 результат: " + expected, function() {
-      assert.equal(pow(x, 3), expected);
-    });
-  }
+  describe("Проверка возведения корректно числа степень n (n >= 0)", function() {
 
-  for (var x = 1; x <= 10; x++) {
-    makeTest(x);
-  }
+    function makeTest(x) {
+      var expected = x * x * x;
+      it("при возведении " + x + " в степень 3 результат: " + expected, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
 
+    for (var x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+
+  });
+
+  describe("Обработка некорректного n", function() {
+
+  	it("При отрицательной степени возвращает NaN", function() {
+  		assert(isNaN(pow(2, -3)));
+  	});
+
+  	it("При отрицательной дробной степени возвращает NaN", function() {
+  		assert(isNaN(pow(2, 1.5)));
+  	});
+
+  });
+
+  it("ноль в нулевой степени возвращает NaN", function() {
+    assert(isNaN(pow(0, 0)), "0 в степени 0 не NaN");
+  });
+  
 });
